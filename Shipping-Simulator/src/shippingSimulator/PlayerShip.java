804ded7217311536daa0b1eb8c4a8891dbdce110
securitygate <benjaminawilk@gmail.com>
2017-07-26 18:42:59 -0400

    Broke out the money and Port functions

--- Shipping-Simulator/src/shippingSimulator/PlayerShip.java
+++ Shipping-Simulator/src/shippingSimulator/PlayerShip.java
@@ -9,22 +9,16 @@ package shippingSimulator;
  *
  * @author benja
  */
-public class PlayerShip {
+public class PlayerShip extends PlayerShipPorts {
 
     private int shipSpeed;
     private int shipMaximumContainers;
     private int shipCurrentContainers;
-    private String currentPort;
-    private String destinationPort;
-    private int playerMoney;
 
     public PlayerShip() {
         this.shipSpeed = 12;
         this.shipCurrentContainers = 0;
         this.shipMaximumContainers = 100;
-        this.currentPort = "LA";
-        this.destinationPort = "";
-        this.playerMoney = 5000;
     }
 
     public int getShipCurrentContainers() {
@@ -34,37 +28,13 @@ public class PlayerShip {
     public int getShipSpeed() {
         return this.shipSpeed;
     }
-    
-    public int getPlayerMoney(){
-        return this.playerMoney;
-    }
 
     public int getShipMaximumContainers() {
         return this.shipMaximumContainers;
     }
 
-    public String getCurrentShipPort() {
-        return this.currentPort;
-    }
-    
-    public String getDestinationShipPort(){
-        return this.destinationPort;
-    }
-
     public void setShipSpeed(int userInputSpeed) {
         this.shipSpeed = userInputSpeed;
-    }
-    
-    public void setPlayerMoney(int userInputMoney){
-        this.playerMoney = userInputMoney;
-    }
-    
-    public void increasePlayerMoney(int userInputIncreaseAmount){
-        this.playerMoney = (this.playerMoney + userInputIncreaseAmount);
-    }
-    
-    public void decreasePlayerMoney(int userInputDecreaseAmount){
-        this.playerMoney = (this.playerMoney - userInputDecreaseAmount);
     }
 
     public void setShipMaximumContainers(int userInputMaxContainers) {
@@ -73,32 +43,6 @@ public class PlayerShip {
 
     public void setShipCurrentContainers(int userInputCurrentContainers) {
         this.shipCurrentContainers = userInputCurrentContainers;
-    }
-
-    public void setCurrentPort(String userInputShipPort) {
-        this.currentPort = userInputShipPort;
-    }
-
-    public void setDestinationPort(String portAssignment) {
-        this.destinationPort = portAssignment;
-    }
-    
-    
-
-    public void portSanityCheck(String userDefinedPort) {
-        if (userDefinedPort.equals(this.destinationPort)) {
-            System.out.println("Sorry, you are already in " + this.currentPort + ". ");
-        } else {
-            this.destinationPort = userDefinedPort;
-        }
-    }
-
-    public String displayDestinationPortMessage() {
-        return "You are currently heading towards " + this.destinationPort + ". ";
-    }
-
-    public String displayCurrentPortMessage() {
-        return "You are currently docked in " + this.currentPort + ". ";
     }
 
     public void getShipStatistics() {
