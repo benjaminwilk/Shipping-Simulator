47851d7acf35186f18973aacefd2b5b13d19def1
benjaminwilk <benjaminawilk@gmail.com>
2018-06-12 21:11:10 -0400

    Slowly making improvements to the hiring system.  Still sort of broken.

--- src/main/java/AvailablePorts.java
+++ src/main/java/AvailablePorts.java
@@ -12,7 +12,7 @@ class Ports{
 	private ContainerVariety portPriceAndCount;
 	private Weather portWeather;
 	private ArrayList<Sailor> SailorsInPort = new ArrayList<Sailor>();
-	
+
 	public Ports(String passedPortName){
 		this.portName = passedPortName;
 		this.portLonLat = new LongitudeLatitude.Builder().title(this.portName).longitude(ParsePortToLon(this.portName)).latitude(ParsePortToLat(this.portName)).build();
@@ -20,149 +20,148 @@ class Ports{
 		this.portWeather = new Weather();
 		setRandomSailors();
 	}
-	
+
 	private void setRandomSailors(){
 		int randomSailorCount = Abstract.GetRandomValue(100, 0);
 		for(int i = 0; i < randomSailorCount; i++){
 			this.SailorsInPort.add(i, new Sailor.Builder().gender().Name().skills().Salary().Nationality().build());
 		}
 	}
-	
+
 	public String GetPortName(){
 		return this.portName;
 	}
-	
+
 	public double GetLatitude(){
 		return this.portLonLat.GetLatitude();
 	}
-	
+
 	public double GetLongitude(){
 		return this.portLonLat.GetLongitude();
 	}
-	
+
 	public void GetContainerCount(){
 		this.portPriceAndCount.displayNameAndQuantity();
 		//return this.portPriceAndCount.GetQuantity();
 	}
-	
+
 	public void GetNameAndPrice(){
 		this.portPriceAndCount.displayNameAndPrice();
 		//return this.portPriceAndCount[].GetPrice();
 	}
-	
+
 	//***************************************************
-	
+
 	public void IncreaseQuantity(int passedPosition, int passedPrice){
 		this.portPriceAndCount.IncreaseQuantity(passedPosition, passedPrice);
 	}
-	
+
 	public void IncreaseQuantity(String passedName, int passedPrice){
 		this.portPriceAndCount.IncreaseQuantity(passedName, passedPrice);
 	}
-	
+
 	public void DecreaseQuantity(int passedPosition, int passedPrice){
 		this.portPriceAndCount.DecreaseQuantity(passedPosition, passedPrice);
 	}
-	
+
 	public void DecreaseQuantity(String passedName, int passedPrice){
 		this.portPriceAndCount.DecreaseQuantity(passedName, passedPrice);
 	}
-	
+
 	public void IncreasePrice(int passedPosition, double passedPrice){
 		this.portPriceAndCount.IncreasePrice(passedPosition, passedPrice);
 	}
-	
+
 	public void IncreasePrice(String passedName, double passedPrice){
 		this.portPriceAndCount.IncreasePrice(passedName, passedPrice);
 	}
-	
+
 	public void DecreasePrice(int passedPosition, double passedPrice){
 		this.portPriceAndCount.DecreasePrice(passedPosition, passedPrice);
 	}
-	
+
 	public void DecreasePrice(String passedName, double passedPrice){
 		this.portPriceAndCount.DecreasePrice(passedName, passedPrice);
 	}
-	
+
 	public int getQuantity(int passedPosition){
 		return this.portPriceAndCount.getQuantity(passedPosition);
 	}
-	
+
 	public int getQuantity(String passedName){
 		return this.portPriceAndCount.getQuantity(passedName);
 	}
-	
+
 	public String getName(int passedPosition){
 		return this.portPriceAndCount.getName(passedPosition);
 	}
-	
+
 	public double getPrice(int passedPosition){
 		return this.portPriceAndCount.getPrice(passedPosition);
 	}
-	
+
 	public double getPrice(String passedName){
 		return this.portPriceAndCount.getPrice(passedName);
 	}
-	
+
 	public void displayNameAndPrice(){
 		this.portPriceAndCount.displayNameAndPrice();
 	}
-	
+
 	public void displayNameAndQuantity(){
 		this.portPriceAndCount.displayNameAndQuantity();
 	}
-	
+
 	public void displayNameQuantityAndPrice(){
 		this.portPriceAndCount.displayNameQuantityAndPrice();
 	}
-	
+
 	public int length(){
 		return this.portPriceAndCount.length();
 	}
-	
+
 	public int getTotalCount(){
 		return this.portPriceAndCount.getTotalCount();
-	}	
-	
-	public void displayAvailableCrew(){
-		System.out.println("Boolin");
-		int[] randomSailorValues = new int[6];
-		for(int i = 0 ; i < randomSailorValues.length; i++){
-			randomSailorValues[i] = Abstract.GetRandomValue(SailorsInPort.size(), 0);
-		}
+	}
+
+	public int getTotalSailorInPortCount(){
+		return this.SailorsInPort.size();
+	}
+
+	public void displayAvailableCrew(int[] sailorValues){
 		System.out.println("----- Available Sailors for Hire -----");
 		for(int i = 0; i < 6; i++){
-			System.out.println(randomSailorValues[i]);
-			System.out.println("Name: " + this.SailorsInPort.get(randomSailorValues[i]).getName());
-			System.out.println("Gender: "+ this.SailorsInPort.get(randomSailorValues[i]).getGender());
-			System.out.println("Nationality: " + this.SailorsInPort.get(randomSailorValues[i]).getNationality());
-			System.out.println("Salary: $" + this.SailorsInPort.get(randomSailorValues[i]).getSalary());
-			System.out.println("Defense: " + this.SailorsInPort.get(randomSailorValues[i]).getDefense());
-			System.out.println("Loading: " + this.SailorsInPort.get(randomSailorValues[i]).getLoading());
-			System.out.println("Steering: " + this.SailorsInPort.get(randomSailorValues[i]).getSteering());
-			System.out.println("Engineering: " + this.SailorsInPort.get(randomSailorValues[i]).getEngineering());
+	//		System.out.println(randomSailorValues[i]);
+			System.out.println("Name: " + this.SailorsInPort.get(sailorValues[i]).getName());
+			System.out.println("Gender: "+ this.SailorsInPort.get(sailorValues[i]).getGender());
+			System.out.println("Nationality: " + this.SailorsInPort.get(sailorValues[i]).getNationality());
+			System.out.println("Salary: $" + this.SailorsInPort.get(sailorValues[i]).getSalary());
+			System.out.println("Defense: " + this.SailorsInPort.get(sailorValues[i]).getDefense());
+			System.out.println("Loading: " + this.SailorsInPort.get(sailorValues[i]).getLoading());
+			System.out.println("Steering: " + this.SailorsInPort.get(sailorValues[i]).getSteering());
+			System.out.println("Engineering: " + this.SailorsInPort.get(sailorValues[i]).getEngineering());
 			System.out.println();
 		}
 	}
-	
+
 	private void removeAvailableSailor(int chosenSailor){
 		this.SailorsInPort.remove(chosenSailor);
 	}
 
 	//***************************************************
-	
+
 	private double ParsePortToLat(String passedPortName){
 		for(int i = 0; i < MenuDisplays.GetPortName().length; i++){
-			if(passedPortName.equals(MenuDisplays.GetPortName(i))){
+			if(passedPortName.equals(MenuDisplays.GetPortName(i + 1))){
 				return MenuDisplays.GetPortLatitude(i);
 			}
 		}
 		return -1.0;
 	}
-	
+
 	private double ParsePortToLon(String passedPortName){
 		for(int i = 0; i < MenuDisplays.GetPortName().length; i++){
-			if(passedPortName.equals(MenuDisplays.GetPortName(i))){
+			if(passedPortName.equals(MenuDisplays.GetPortName(i + 1))){
 				return MenuDisplays.GetPortLongitude(i);
 			}
 		}
@@ -173,19 +172,27 @@ class Ports{
 
 public class AvailablePorts{
 	ArrayList <Ports> PortLocations = new ArrayList<Ports>();
-	
+
 	public AvailablePorts(String[] passedPorts){
 		for(int i = 0; i < passedPorts.length; i++){
 			this.PortLocations.add( new Ports(passedPorts[i]));
 		}
 	}
 
-	public void displayAvailableCrew(String passedName){
-		this.PortLocations.get(Abstract.convertStringToInt(passedName, MenuDisplays.GetPortName())).displayAvailableCrew();
+	public void getSpecificSailor(int specificSailor){
+		
 	}
-	
-	public void displayAvailableCrew(int passedValue){
+
+	public void displayAvailableCrew(String passedName, int[] sailorValues){
+		this.PortLocations.get(Abstract.convertStringToInt(passedName, MenuDisplays.GetPortName())).displayAvailableCrew(sailorValues);
+	}
+
+	public int getTotalSailorInPortCount(String passedName){
+			return this.PortLocations.get(Abstract.convertStringToInt(passedName, MenuDisplays.GetPortName())).getTotalSailorInPortCount();
+	}
+
+/*	public void displayAvailableCrew(int passedValue){
 		this.PortLocations.get(passedValue).displayAvailableCrew();
-	}
-	
+	}*/
+
 }
--- src/main/java/SailorMenu.java
+++ src/main/java/SailorMenu.java
@@ -17,7 +17,7 @@ public class SailorMenu{
 	//	displayAvailableCrew();
 	//	hireCrewDecision(playerObject);
 	}
-	
+
 	private void copyMembersFromPlayer(Boat playerObject){
 		if(playerObject.AnySailors()){
 			for(int i = 0; i < playerObject.getSailorCount(); i++){
@@ -25,30 +25,30 @@ public class SailorMenu{
 			}
 		}
 	}
-	
+
 	private void displayPlayerSailors(Boat playerObject){
 		System.out.println("----- Your Hired Sailors -----");
 		if(playerObject.AnySailors()){
 			for(int x = 0; x < getSailorCount(); x++){
 				System.out.println("Name: " + this.SailorsAboard.get(x).getName());
 				System.out.println("Gender: "+ this.SailorsAboard.get(x).getGender());
-				System.out.println("Nationality: " + this.SailorsAboard.get(x).getNationality()); 
+				System.out.println("Nationality: " + this.SailorsAboard.get(x).getNationality());
 				System.out.println("Salary: $" + this.SailorsAboard.get(x).getSalary());
 				System.out.println("Defense: " + this.SailorsAboard.get(x).getDefense());
 				System.out.println("Loading: " + this.SailorsAboard.get(x).getLoading());
 				System.out.println("Steering: " + this.SailorsAboard.get(x).getSteering());
 				System.out.println("Engineering: " + this.SailorsAboard.get(x).getEngineering() + "\n");
 			}
-			
+
 		} else {
 			System.out.println("\nYou don't have any crewmembers!\n");
 		}
 	}
-	
+
 	private int getSailorCount(){
 		return this.SailorsAboard.size();
 	}
-	
+
 /*	private void generateCrew(){
 		//System.out.println("----- Available Sailors for Hire -----");
 		if(generatorStatus == false){
@@ -58,20 +58,29 @@ public class SailorMenu{
 			generatorStatus = true;
 		}
 	}*/
-	
+
 
 	//	Abstract.PressAnyKey();
-	
+
 		//System.out.println(AvailableCrewmen.get(displayCrewmenInformation() ));
-	
+
+		private int[] rollForSailors(Boat playerObject, AvailablePorts ports){
+			int[] randomSailorValues = new int[6];
+			for(int i = 0 ; i < randomSailorValues.length; i++){
+				randomSailorValues[i] = Abstract.GetRandomValue(ports.getTotalSailorInPortCount(playerObject.GetCurrentName()), 0);
+			}
+			return randomSailorValues;
+		}
+
 
 	private void hireOrFire(Boat playerObject, AvailablePorts ports){
+		int[] sailorValues = rollForSailors(playerObject, ports);
 		System.out.print("Are you looking to add sailors, or make changes to your current roster(Hire/fire/totals/Back): ");
 		String hireChoice = Abstract.ScannerString();
 		if(hireChoice.equalsIgnoreCase("Hire") || hireChoice.equalsIgnoreCase("h") || hireChoice.equalsIgnoreCase("1")){
 			displayPlayerSailors(playerObject);
-			ports.displayAvailableCrew(playerObject.GetCurrentName());
-			hireCrewDecision(playerObject);
+			ports.displayAvailableCrew(playerObject.GetCurrentName(), sailorValues);
+			hireCrewDecision(playerObject, sailorValues, ports);
 		}
 		if(hireChoice.equalsIgnoreCase("total") || hireChoice.equalsIgnoreCase("t") || hireChoice.equalsIgnoreCase("3")){
 			playerObject.recalculateLevels();
@@ -81,9 +90,9 @@ public class SailorMenu{
 			displayPlayerSailors(playerObject);
 			fireDecision(playerObject);
 		}
-		
+
 	}
-	
+
 	private void fireDecision(Boat playerObject){
 		if(playerObject.getSailorCount() > 0){
 			System.out.print("Choose which employee to let go (1,2,3... | 0 to quit): ");
@@ -97,12 +106,13 @@ public class SailorMenu{
 			System.out.println("\nYou don't have any employees to fire.\n");
 		}
 	}
-	
-	private void hireCrewDecision(Boat playerObject){
+
+	private void hireCrewDecision(Boat playerObject, int[] sailorValues, AvailablePorts ports){
 		System.out.print("Which sailors would you like to hire?(1,2,3 | B to go back): ");
 		char sailorChoice = Abstract.ScannerChar();
 		if(Character.isDigit(sailorChoice)){
-	//		playerObject.addSailor(this.AvailableSailors.get(Character.getNumericValue(sailorChoice) - 1));
+			//playerObject.addSailor(this.AvailableSailors.get(Character.getNumericValue(sailorChoice) - 1));
+			//playerObject.addSailor(ports.)
 			playerObject.displaySailorCount();
 		//	removeAvailableSailor(Character.getNumericValue(sailorChoice) - 1);
 		}
@@ -111,6 +121,6 @@ public class SailorMenu{
 			playerObject.IncreaseDate();
 		}
 	}
-	
-	
+
+
 }
