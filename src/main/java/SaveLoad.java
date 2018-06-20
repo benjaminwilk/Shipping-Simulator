ec6075375aeb47e9c2752fec5ad75394ce7bae76
Benjamin Wilk <Benjaminawilk@gmail.com>
2018-06-19 22:23:45 -0400

    Added some keys for the save values.

--- src/main/java/SaveLoad.java
+++ src/main/java/SaveLoad.java
@@ -7,29 +7,29 @@ import java.io.*;
 public class SaveLoad{
 
 	public SaveLoad(Boat playerObject){
-		
+
 	}
-	
+
 	public void SaveGame(Boat playerObject){
 		try {
 			FileOutputStream fileCreator = new FileOutputStream("C:\\Program Files\\Java\\Boat.sav");
 			ObjectOutputStream fileWriter = new ObjectOutputStream(fileCreator);
 			fileWriter.writeObject(playerObject.GetDate());
-			fileWriter.writeObject(playerObject.GetShipName());
-			fileWriter.writeObject(playerObject.GetMaximumContainers());
-			fileWriter.writeObject(playerObject.GetCurrentFuel());
-			fileWriter.writeObject(playerObject.GetMaximumFuel());
-			fileWriter.writeObject(playerObject.GetMoney());
-			fileWriter.writeObject(playerObject.GetCurrentName());
+			fileWriter.writeObject("Shipname=" + playerObject.GetShipName());
+			fileWriter.writeObject("MaxContainers=" + playerObject.GetMaximumContainers());
+			fileWriter.writeObject("CurrentFuel=" + playerObject.GetCurrentFuel());
+			fileWriter.writeObject("MaxFuel=" + playerObject.GetMaximumFuel());
+			fileWriter.writeObject("Money=" + playerObject.GetMoney());
+			fileWriter.writeObject("Location=" + playerObject.GetCurrentName());
 			fileWriter.close();
 			fileCreator.close();
 			System.out.println("Saved!");
 		}catch(IOException i){
 			i.printStackTrace();
 		}
-			
+
 	}
-	
+
 	public void Loader(){
 		Boat playerObject = null;
 		try{
